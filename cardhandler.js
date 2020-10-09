5f23f1ac27b9bb372a7ebbf124504e4575b39991
Sterben-01 <49880952+Sterben-01@users.noreply.github.com>
2020-10-09 17:04:08 +0800

    Update cardhandler.js

--- cardhandler.js
+++ cardhandler.js
@@ -12,7 +12,7 @@ function addCard(cardID)
         <img class="card-img-top" src="https://cdn2.iconfinder.com/data/icons/social-media-square-set/960/Twitter_Sq-512.png" alt="Twitter Logo">
         <div class="card-body">
             <h5 class="card-title text-right">Tweet</h5>
-            <p class="card-text text-right">Sample tweet will go here.</p>
+            <p class="card-text text-right">Sample tweet will go here!</p>
         </div>
         `;
     editCard.appendChild(tweet);
